9807e69c30cfc5478822e9afc625074a647d8bdd
nugunga <nugunga8086@gmail.com>
2022-01-09 03:41:34 +0900

    main

--- QuantumCompiler/Modules/String/Sources/String.c
+++ QuantumCompiler/Modules/String/Sources/String.c
@@ -1,12 +1,14 @@
 
 #include "String.h"
 #include "Private_String.h"
+#include "Private_StringLib.h"
 #include "ProgramManager.h"
 
 #include <locale.h>
 #include <stdio.h>
 
 struct StringMethod StringMethod;
+struct StringLibMethod StringLibMethod;
 
 // TODO 각종 전역 변수 및 모듈 초기화 함수를 통합
 
@@ -38,5 +40,23 @@ void StringModule_Initialized() {
   StringMethod.UnConst = String_UnConst;
   StringMethod.Destructor = String_Destructor;
   StringMethod.IndexFor = String_IndexFor;
+
+  StringLibMethod.Extract = String_Extract;
+  StringLibMethod.Format = String_Format;
+  StringLibMethod.IsAlpha = String_IsAlpha;
+  StringLibMethod.IsAlphaDigit = String_IsAlphaDigit;
+  StringLibMethod.IsBinary = String_IsBinary;
+  StringLibMethod.IsControl = String_IsControl;
+  StringLibMethod.IsDecimal = String_IsDecimal;
+  StringLibMethod.IsDigit = String_IsDigit;
+  StringLibMethod.IsHex = String_IsHex;
+  StringLibMethod.IsLower = String_IsLower;
+  StringLibMethod.IsOctal = String_IsOctal;
+  StringLibMethod.IsSpace = String_IsSpace;
+  StringLibMethod.IsUpper = String_IsUpper;
+  StringLibMethod.Notation = String_Notation;
+  StringLibMethod.Pattern = String_Pattern;
+  StringLibMethod.Reverse = String_Reverse;
+
   setlocale(LC_ALL, "");
 }
--- QuantumCompiler/Modules/String/Sources/StringLib.c
+++
@@ -1,8 +0,0 @@
-
-#include "StringLib.h"
-#include "Chs.h"
-#include "Private_StringLib.h"
-#include <stdio.h>
-#include <wchar.h>
-
-// TODO 파일이 너무 커질경우 분할
