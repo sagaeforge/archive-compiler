c67e70549bd9f44860b6a86d24101f690d54db60
nugunga8086 <60589396+nugunga8086@users.noreply.github.com>
2022-01-10 10:52:49 +0900

    Update String_Split.c

--- QuantumCompiler/Modules/String/Sources/StringMethod/String_Split.c
+++ QuantumCompiler/Modules/String/Sources/StringMethod/String_Split.c
@@ -1,11 +1,36 @@
 
+#include "Private_StringAry.h"
 #include "Private_String.h"
+#include "Private_StringLib.h"
 #include "ProgramManager.h"
 
-// TODO 나중에 구현
+// 오류 테스트 나중에 구현
 
 StringAry*
 String_Split(String* Self, String* Value)
 {
-  return NULL;
+  StringAry *Ary = StringAryConstructor(0);
+  Length Cnt = String_Count(Self, Value);
+  
+  if(Cnt == 0)
+  {
+    StringAry_Push(Ary, Self);
+    return Ary;
+  }
+  
+  wcs *temp = __WcsCreate(Self->Length);
+  
+  Index Start = 0;
+  int i;
+  for (i = 0; i < Cnt; i++)
+  {
+    Index index = String_IndexFor(Self, Value, i);
+    StringAry_Push(
+      Ary,
+      String_Extract(Self, Start, index)
+    )
+    Start = index + Value->Length;
+  }
+  
+  return Ary;
 }
