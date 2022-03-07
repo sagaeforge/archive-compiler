4b3aa89997f22fb8b6f96bc1867fb9e76d93e541
nugunga8086 <nugunga8086@gmail.com>
2022-03-07 18:06:07 +0900

    main

--- Sources/nDigitAry/DigitAry_Pop.c
+++ Sources/nDigitAry/DigitAry_Pop.c
@@ -4,6 +4,9 @@
 DEFUALT_DIGIT
 DigitAry_Pop(nDigitAry_ptr pSelf)
 {
+  if (pSelf->m_Length == 0)
+    return -1;
+
   DEFUALT_DIGIT _Ret = DigitAry_get(pSelf, pSelf->m_Length);
   DigitAry_Remove(pSelf, pSelf->m_Length);
   return _Ret;
--- Sources/nRegExp/RegExp_Analysis.c
+++ Sources/nRegExp/RegExp_Analysis.c
@@ -8,9 +8,84 @@
 nRegExpResult_t*
 RegExp_Analysis(const nString_t* pSelf, const nRegExp_t* pRegExp)
 {
-  // 선두 접두어 분석 파트
+  Index_t _i, _RegExpPointer = 0;
+  for (_i = 0; _i < pSelf->m_Length; _i++) {
 
-  // 의미 해석 파트
+    // RegExp 분석
+    while (true) {
+      switch (pRegExp->m_Pattern->m_Value[_RegExpPointer]) {
+        case '^':
+          break;
+        case '$':
+          break;
+        case '.':
+          break;
+        case '[':
+          break;
+        case '-':
+          break;
+        case '(':
+          break;
+        case '*':
+          break;
+        case '+':
+          break;
+        case '?':
+          break;
+        case '{':
+          break;
+        case '\\':
+          switch (pRegExp->m_Pattern->m_Value[_RegExpPointer + 1]) {
+            case 'w':
+              break;
+            case 'W':
+              break;
+            case 'd':
+              break;
+            case 'D':
+              break;
+            case 'b':
+              break;
+            case 'B':
+              break;
+            case 'A':
+              break;
+            case 'Z':
+              break;
+            case '^':
+              break;
+            case '$':
+              break;
+            case '.':
+              break;
+            case '[':
+              break;
+            case '-':
+              break;
+            case '(':
+              break;
+            case '*':
+              break;
+            case '+':
+              break;
+            case '?':
+              break;
+            case '{':
+              break;
+            case '\\':
+              break;
+            case '/':
+              break;
+
+            default:
+              break;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
 
   return NULL;
 }
